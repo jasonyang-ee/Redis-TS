8ac6fdd16d28842f065bc871e8dace9058e6ae57
Jason Yang <git@jasony.org>
2024-04-29 00:53:25 -0700

    Add: measure performance

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use redis::Commands;
-use redis_ts::{TsCommands, TsOptions, TsDuplicatePolicy, TsRange, TsRangeQuery};
+use redis_ts::{TsCommands, TsOptions, TsDuplicatePolicy, TsRange, TsRangeQuery, TsInfo};
 use std::thread;
 use std::time::{Duration, SystemTime};
 use sysinfo::{System, RefreshKind, CpuRefreshKind};
@@ -10,7 +10,7 @@ fn main() {
     let mut con = client.get_connection().unwrap();
 
 	let my_opts = TsOptions::default()
-	.retention_time(60000)
+	.retention_time(60000000)
 	.uncompressed(false)
 	.duplicate_policy(TsDuplicatePolicy::Last);
 
@@ -22,14 +22,19 @@ fn main() {
 		let _:() = con.ts_create("cpu_usage", my_opts).unwrap();
 	}
 
-	// Loop forever
-	loop {
-		// Update the CPU usage
+	// each loop is 10ms, loop for 1 hour
+	for counts in 0..2000 {
 		update_cpu_usage(&mut con);
 
-		// Print the CPU usage
-		print_cpu_usage(&mut con);
+		// print counts every 1 second
+		if counts % 100 == 0 {
+			let info:TsInfo = con.ts_info("cpu_usage").unwrap();
+			println!("Counts:{} - Samples:{} - Memory:{}", counts, info.total_samples, info.memory_usage);
+		}
 	}
+	
+	// Print all CPU usage in the end
+	print_cpu_usage(&mut con);
 }
 
 fn print_cpu_usage(con: &mut redis::Connection) {
@@ -65,13 +70,13 @@ fn update_cpu_usage(con: &mut redis::Connection) {
 	let cpu_usage_i: i32 = (cpu_usage * 100.0) as i32;
 
 	// Get the current timestamp
-	let timestamp = SystemTime::now()
-	.duration_since(SystemTime::UNIX_EPOCH).expect("REASON")
-	.as_secs() as i64;
+	// let timestamp = SystemTime::now()
+	// .duration_since(SystemTime::UNIX_EPOCH).expect("REASON")
+	// .as_secs() as i64;
 
 	// Store the CPU usage in Redis
-	let _:() = con.ts_add("cpu_usage", timestamp, cpu_usage_i).expect("REASON");
+	let _:() = con.ts_add_now("cpu_usage", cpu_usage_i).expect("REASON");
 
 	// Sleep for 1 second
-	thread::sleep(Duration::from_millis(100));
+	thread::sleep(Duration::from_millis(1));
 }
